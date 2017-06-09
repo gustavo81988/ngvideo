bd6c43308d1aeeec5fcdfc51351534f55e2d24ec
Gustavo Ramírez <gustavo81988@gmail.com>
2017-06-09 14:45:14 -0400

    Mapping Urls with RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 
@@ -16,7 +17,8 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
